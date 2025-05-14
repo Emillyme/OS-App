e5bcbca12dbd6a35da7be217900ff358258779fb
emillyme <emill.ymello212@gmail.com>
2025-05-14 10:57:29 -0300

    update

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -3,9 +3,13 @@ import React from 'react'
 type Props = {}
 
 function Login({}: Props) {
+  
   return (
-    <div>
-        Login
+    <div className='flex flex-col w-[300px]'>
+        <p>Login</p>
+        <input type="text" className='border mb-2' placeholder='username' />
+        <input type="text" className='border' placeholder='password'/>
+        <button className='border bg-black text-white mt-5 cursor-pointer'>Login</button>
     </div>
   )
 }
